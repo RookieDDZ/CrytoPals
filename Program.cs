79fede28cfa3dec6e189cb2e570d0bb111c55c1a
Brian Chabot <chabotb1@gmail.com>
2016-11-20 20:01:04 -0500

    initial upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CrytoPal_1_1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//define variables
+			string hexData = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+			byte[] byteData;
+			string finalBase64;
+
+			hexData = hexData.Replace("-", "");
+
+			byte[] resultantArray = new byte[hexData.Length / 2];
+
+			for (int i = 0; i < resultantArray.Length; i++)
+			{
+				resultantArray[i] = Convert.ToByte(hexData.Substring(i * 2, 2), 16);
+			}
+			byteData = resultantArray;
+
+			finalBase64 = Convert.ToBase64String(byteData);
+			Console.WriteLine(finalBase64);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CrytoPals
 Cryptopals Crypto Challenges
+
+I decided to work on these challenges to help understand cryptography and keep the mind fresh.
